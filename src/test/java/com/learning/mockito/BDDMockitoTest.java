1e0e888d0b945d5010a065227cfafbf4655e3782
chakrabortyanshu <chakrabortyanshu@gmail.com>
2020-09-22 22:00:21 -0500

    finishing the Mockito 3.5.11 Javadoc

--- src/test/java/com/learning/mockito/BDDMockitoTest.java
+++ src/test/java/com/learning/mockito/BDDMockitoTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.times;
+
 public class BDDMockitoTest {
 
     @Test
@@ -18,5 +20,24 @@ public class BDDMockitoTest {
 
         BDDMockito.verify(person).getName();
 
+    }
+
+    @Test
+    void bddMockingTest2() {
+        Person person = Mockito.mock(Person.class);
+
+        //Given
+        BDDMockito.given(person.getName()).willReturn("XYZ");
+
+        //When
+        String name = person.getName();
+        String name2 = person.getName();
+
+        //Then
+        Assertions.assertEquals("XYZ",name);
+        Assertions.assertNotEquals("EFAB",name2);
+
+        //Then
+        BDDMockito.then(person).should(times(2)).getName();
     }
 }
